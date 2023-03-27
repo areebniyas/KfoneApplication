882c679096fe1fd0dab8c33eed964458109ed1fc
Nipun Pramuditha <pramudithanipun9@gmail.com>
2023-03-27 13:34:31 +0530

    add relevent components

---
+++ src/components/DisplayComponent/Display.js
@@ -0,0 +1,11 @@
+function DisplayComponent() {
+    return (
+      <div>
+        <h2>Display Component</h2>
+        <p>This is the content for the Display Component.</p>
+      </div>
+    )
+  }
+  
+export default DisplayComponent
+  
