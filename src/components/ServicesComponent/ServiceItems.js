3c28c7bac1f420996d51f44e84bf8beaecc2a8e1
Janidhu Wijetunga <janidhu@wso2.com>
2023-03-28 11:00:19 +0530

    added services

---
+++ src/components/ServicesComponent/ServiceItems.js
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+} from "@mui/material";
+import Button from "@mui/material/Button";
+import Fab from "@mui/material/Fab";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { padding } from "@mui/system";
+
+function ServiceProduct({ product }) {
+  return (
+    <Card sx={{ width: 300, height: 600, position:'relative' }}>
+      <CardMedia
+        component="img"
+        image="/images/sim.jpg"
+        height="300"
+        alt={product.Name}
+      />
+      <CardContent
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <div style={{width:'95%'}}>
+          <Typography variant="h6" sx={{ marginBottom: 1 }}>
+            {product.Name}
+          </Typography>
+          <Typography variant="h5" sx={{ marginBottom: 1 }}>
+            Rs.{product.Price}
+          </Typography>
+          <Typography variant="subtitle1" sx={{ overflow: 'hidden', marginBottom: 1, maxWidth: '300px', whiteSpace: 'nowrap'}}>
+            {product.Description}
+          </Typography>
+          <Typography variant="h5" sx={{ marginBottom: 3 }}>
+            {product.Manufacturer}
+          </Typography>
+          <CardActions>
+            {/* <Fab aria-label="like">
+              <FavoriteIcon />
+            </Fab> */}
+            <Fab aria-label="add-to-cart" style={{marginRight:'125px'}}>
+              <FavoriteBorderIcon/>
+            </Fab>
+            <Fab aria-label="add-to-cart">
+              <AddShoppingCartIcon />
+            </Fab>
+          </CardActions>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default ServiceProduct;
--- src/components/ServicesComponent/Services.js
+++ src/components/ServicesComponent/Services.js
@@ -1,9 +1,19 @@
+import Product from "./ServiceItems"
+import productsData from "../../data/sample-services.json"
+import { Grid } from "@mui/material"
+import { Box } from "@mui/system";
+
 function ServicesComponent() {
     return (
-      <div>
-        <h2>Services Component</h2>
-        <p>This is the content for the Services Component.</p>
-      </div>
+      <Box className='display-box' sx={{ margin:'50px', display: 'flex', overflowX: 'scroll', scrollBehavior: 'smooth' }}>
+          {/* <Grid container spacing={2}> */}
+          {productsData.map((product) => (
+            <Box sx={{ minWidth: 300, maxWidth: 300, marginRight: 2 }}>
+              <Product key={product.Name} product={product} />
+            </Box>
+          ))}
+          {/* </Grid> */}
+      </Box>
     )
   }
   
