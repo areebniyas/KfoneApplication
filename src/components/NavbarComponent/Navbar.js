40b03bcf3642497dfcf2626b2e9d4f31b7937f22
areebniyas <areebniyas@gmail.com>
2023-03-28 13:32:21 +0530

    adding asgardeo signin and signout

--- src/components/NavbarComponent/Navbar.js
+++ src/components/NavbarComponent/Navbar.js
@@ -1,9 +1,10 @@
+import { useSession, getSession, signIn, signOut } from 'next-auth/react'
 import Link from 'next/link'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import Image from 'next/image';
 
 const Navbar = () => {
-
+  const { data: session, status } = useSession()
   const user = null;
   return (
     !user ? (
@@ -11,14 +12,29 @@ const Navbar = () => {
         <ul className='navbar-links'>
         <Image src='/logo.svg' alt="Logo" className='logo' width={104} height={168}/>
           <li>
-            <Link href="/signin">
-              <div>SignIn</div>
-            </Link>
-          </li>
-          <li>
-            <Link href="/signup">
-              <div>SignUp</div>
-            </Link>
+            {
+              session ? 
+              (
+                <button
+                  className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                  aria-label="Sign out"
+                  title="Sign out"
+                  onClick={() => signOut({ callbackUrl: "http://localhost:3000/" })}
+                >
+                  Sign Out
+                </button>
+              ) :
+              (
+                <button
+                  className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                  aria-label="Sign up"
+                  title="Sign up"
+                  onClick={() => signIn("asgardeo", { callbackUrl: "http://localhost:3000/" })}
+                >
+                  Sign In
+                </button>
+              )
+            }
           </li>
           
         </ul>
---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,34 @@
+import NextAuth from "next-auth"
+
+export default NextAuth({
+    providers: [
+      {
+        id: "asgardeo",
+        name: "Asgardeo",
+        type: "oauth",
+        clientId: process.env.ASGARDEO_CLIENT_ID,
+        clientSecret: process.env.ASGARDEO_CLIENT_SECRET,
+        wellKnown: "https://api.asgardeo.io/t/" + process.env.ASGARDEO_ORGANIZATION_NAME + "/oauth2/token/.well-known/openid-configuration",
+        authorization: {
+          params: { scope: process.env.ASGARDEO_SCOPES || "openid email profile" } 
+        },
+        idToken: true,
+        checks: ["pkce", "state"],
+        profile(profile) {
+          return {
+            id: profile?.sub,
+            name: profile?.given_name,
+            email: profile?.email
+          }
+        },
+        httpOptions: {
+          timeout: 40000,
+        },
+      },
+    ],
+    secret: process.env.SECRET,
+  
+    session: {
+      strategy: "jwt",
+    },
+  })
