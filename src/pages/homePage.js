d9365e42734876bface3545be60bd3e7e3fa98fb
Ilma Basheer <117967359+bfilma1@users.noreply.github.com>
2023-03-29 15:20:07 +0530

    create user record in DB for new users

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,10 +1,11 @@
 import DisplayComponent from '../components/DisplayComponent/Display'
 import ServicesComponent from '../components/ServicesComponent/Services'
 import AddToCartComponent from '../components/AddToCartComponent/AddToCart'
-import { useState } from 'react';
+import { useEffect, useState,useRef  } from 'react';
 import { Icon } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 import ButtonGroup from '@mui/material/ButtonGroup';
 
 function HomePage() {
@@ -14,6 +15,55 @@ function HomePage() {
   const handleButtonClick = (buttonName) => {
     setActiveButton(buttonName);
   };
+
+  //post user details
+  const dataFetchedRef = useRef(false);
+
+  useEffect(() => {
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+
+    async function getUserInfo(){
+      const response = await fetch('http://localhost:3000/api/userInfo');
+      const json = await response.json();
+      return json;
+    }
+
+    async function postUser(userDetails) {
+      const response = await fetch('http://localhost:3000/api/users',{
+        method: "POST",
+        body: JSON.stringify({
+          username: userDetails.data["username"],
+          sub: userDetails.data["sub"],
+          family_name: userDetails.data["family_name"],
+          given_name: userDetails.data["given_name"],
+          cart: [],
+          favourites: [],
+          points: 0
+        }),
+      })
+    }
+
+    async function checkUser(sub){
+      const response = await fetch(`http://localhost:3000/api/getUser?userId=${sub}`)
+      const data = await response.json()
+      return data;
+    } 
+
+    async function fetchData() {
+      const userDetails = await getUserInfo();
+      const dbDetails = await checkUser(userDetails.data["sub"]);
+      console.log(JSON.stringify(userDetails))
+      if(dbDetails.message.length>0){
+        console.log("User exists")
+        return
+      }
+      console.log("DBDetails"  + JSON.stringify(dbDetails));
+      postUser(userDetails);
+    }
+    
+    fetchData();
+  }, []);
 
   const buttons = [
     <Button className='btn-services' onClick={() => handleButtonClick('display')}>Display</Button>,
