cd2ba8597437c779c7b2adced67e0d5be0a39c54
areebniyas <areebniyas@gmail.com>
2023-03-31 08:57:38 +0530

    revert devices api

--- src/pages/api/devices.js
+++ src/pages/api/devices.js
@@ -4,41 +4,17 @@ export default async (req, res) => {
     try {
         // connect to the database
         let { db } = await connectToDatabase();
-        
-        switch(req.method){
-            // case "POST":
-            //     let bodyObject = JSON.parse(req.body);
-            //     let myPost = await db.collection("users").insertOne(bodyObject);
-            //     res.json(myPost.ops[0]);
-            //     break;
-            case "GET":
-                 // fetch the posts
-                 let devices = await db
-                    .collection('devices')
-                    .find({  })
-                    .sort({ published: -1 })
-                    .toArray();
-                // return the posts
-                res.json({
-                    message: JSON.parse(JSON.stringify(devices)),
-                    success: true,
-                });
-                break;
-            // case "PUT":
-            //     let sub = req.query.sub;
-            //     let field = req.query.field;
-            //     let value = req.body;
-            //     var obj = {};
-            //     obj[field] = value;
-
-            //     const result = await db 
-            //         .collection('users')
-            //         .updateOne({ sub: sub },{ $set:  obj  });
-                
-            //       res.status(200).json({ message: 'Update successful' });
-        }
-
-        return res.json;
+        // fetch the posts
+        let posts = await db
+            .collection('devices')
+            .find({})
+            .sort({ published: -1 })
+            .toArray();
+        // return the posts
+        return res.json({
+            message: JSON.parse(JSON.stringify(posts)),
+            success: true,
+        });
     } catch (error) {
         // return the error
         return res.json({
